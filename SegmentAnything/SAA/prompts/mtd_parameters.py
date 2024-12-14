d4b16d93abd5596d593506174e29a84e43b63035
sybeam27 <sypark1452@o.cnu.ac.kr>
2024-12-14 17:34:58 +0000

    upload

---
+++ SegmentAnything/SAA/prompts/mtd_parameters.py
@@ -0,0 +1,11 @@
+manual_prompts = {
+    'Magnetic': [
+        ['black hole. blow hole. break. crack. fray. uneven.', 'mtd'],
+        ['defect.', 'mtd'],
+    ],
+
+}
+
+property_prompts = {
+    'Magnetic': 'the image of mtd have 1 dissimilar mtd, with a maximum of 5 anomaly. The anomaly would not exceed 0.9 object area. ',
+}
